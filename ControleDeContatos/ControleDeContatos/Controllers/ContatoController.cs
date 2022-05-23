2759a3ea25bc277f843fbe2fad6206d998c4f515
BigFabiano <nbscbs@gmail.com>
2022-05-22 22:15:47 -0300

    Meu primeiro MVC

---
+++ ControleDeContatos/ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index() //iaction das paginas
+        {
+            return View();
+        }
+
+        public IActionResult Criar() //iaction das paginas
+        {
+            return View();
+        }
+
+        public IActionResult Editar() //iaction das paginas
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao() //iaction das paginas
+        {
+            return View();
+        }
+    }
+}
